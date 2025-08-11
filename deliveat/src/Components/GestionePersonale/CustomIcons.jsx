bb89fdba678095f4c842de18e8baf4b925dd49e1
Robotpazzo <maxprime512@gmail.com>
2025-08-11 16:24:25 +0200

    init

--- deliveat/src/Components/GestionePersonale/CustomIcons.jsx
+++ deliveat/src/Components/GestionePersonale/CustomIcons.jsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import SvgIcon from '@mui/material/SvgIcon';
+//import SvgIcon from '@mui/material/SvgIcon';
 import DelivEat_logo from '../../Images/DelivEat_logo.png';
 
 import './GestionePersonale.css';
