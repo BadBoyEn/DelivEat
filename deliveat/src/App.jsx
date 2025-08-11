bbc42605d74ca9def04d70c283839c71d524d49f
Robotpazzo <maxprime512@gmail.com>
2025-08-11 15:39:57 +0200

    init

--- deliveat/src/App.jsx
+++ deliveat/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
