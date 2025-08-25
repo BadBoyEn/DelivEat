dc8dfe649e13a486dbf99ccadb70afd80922dd39
Tony-ScarFace <scaranoantonio2004@gmail.com>
2025-08-25 13:38:38 +0200

    init
    init

--- deliveat/src/App.jsx
+++ deliveat/src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignUpPage from './pages/SignUpPage';
-import LogInPage from './pages/LogInPage';
-import HomePage from './pages/HomePage';
-import FAQPage from './pages/FAQPage';
-import DashBoardPage from './pages/DashBoardPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+// Pagine (i tuoi file)
+import LogInPage from './pages/LogInPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import DashBoardPage from './pages/DashBoardPage.jsx'
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/faq" element={<FAQPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+
+        {/* pagine principali */}
         <Route path="/login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/dashboard" element={<DashBoardPage />} />
+
+        {/* 404 */}
+        <Route path="*" element={<h2 style={{padding:16}}>Pagina non trovata</h2>} />
       </Routes>
     </Router>
   )
