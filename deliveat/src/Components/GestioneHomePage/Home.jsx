c793a4b65a680866619500efbf5ada93498bc194
domimorea <domenicomorea02@gmail.com>
2025-08-25 16:56:56 +0200

    init

--- deliveat/src/Components/GestioneHomePage/Home.jsx
+++ deliveat/src/Components/GestioneHomePage/Home.jsx
@@ -28,7 +28,7 @@ export default function Home() {
           <Button variant='contained' color="secondary">Chi siamo</Button>
           <Button variant='contained' color="secondary">Come funziona</Button>
           <Button component={Link} to="/home/faq" variant='contained' color="secondary">FAQ</Button>
-          <Button variant={Link} to
+          <Button variant='contained' color="secondary">Contatti</Button>
           <Button variant='contained' color="secondary">Info Legali</Button>
         </Box>
         <Box className="color-switch-home">
