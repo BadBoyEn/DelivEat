011b667cc10e25a2148cf9e8b3bc2b937c30ef67
domimorea <domenicomorea02@gmail.com>
2025-08-26 09:58:05 +0200

    init

---
+++ deliveat/src/pages/ContattiPage.jsx
@@ -0,0 +1,7 @@
+import AppTheme from '../theme/AppTheme.jsx'
+import Contatti from '../Components/GestioneHomePage/GestioneContatti/Contatti.jsx'
+export default function LogInPage () {
+    return <AppTheme>
+        <Contatti/>
+    </AppTheme>
+}
