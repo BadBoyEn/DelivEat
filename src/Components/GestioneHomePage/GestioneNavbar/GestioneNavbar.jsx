a603f278bc6b8a491226a51b19c3342d5c442f8b
Robotpazzo <maxprime512@gmail.com>
2025-09-15 15:33:03 +0200

    init

--- src/Components/GestioneHomePage/GestioneNavbar/GestioneNavbar.jsx
+++ src/Components/GestioneHomePage/GestioneNavbar/GestioneNavbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { AppBar, Toolbar, Button, Box, IconButton, Menu, MenuItem, Typography } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Link } from "react-router-dom";
-import SistemarkIcon from '../../GestioneIcons/SistemarkIcon.jsx';
+import SitemarkIcon from '../../GestioneIcons/SistemarkIcon.jsx';
 import ColorModeSelect from '../../../theme/ColorModeSelect.jsx';
 
 export default function GestioneNavbar({ children }) {
@@ -14,37 +14,64 @@ export default function GestioneNavbar({ children }) {
 
   const pages = [
     { label: "Home", to: "/" },
-    { label: "Chi siamo", to: "/home/chisiamo" },
-    { label: "Come funziona", to: "/home/comefunziona" },
-    { label: "Contatti", to: "/home/contatti" },
+    { label: "Chi Siamo", to: "/home/chisiamo" },
+    { label: "Come Funziona", to: "/home/comefunziona" },
     { label: "FAQ", to: "/home/faq" },
-    { label: "Info legali", to: "/home/infolegali" }
+    { label: "Contatti", to: "/home/contatti" },
+    { label: "Info Legali", to: "/home/infolegali" },
   ];
 
   return (
-    <Box className="layout">
-      <AppBar position="static" color="transparent" elevation={0} className="navbar">
-        <Toolbar className="toolbar">
-          <Box className="left">
-            <SistemarkIcon className="logo" />
-          </Box>
+    <Box className="page">
+      <AppBar elevation={0} position="relative" color="primaryHome" className="appbar-custom">
+        <Toolbar className="toolbar-custom">
+          {/* -- COMMENTO -- Switch tema fissato a SINISTRA */}
+          <ColorModeSelect className="color-switch-home" />
 
-          <Box className="center">
-            {pages.map(p => (
-              <Button key={p.to} component={Link} to={p.to} className="nav-btn">
-                {p.label}
+          {/* -- COMMENTO -- Menu desktop al centro */}
+          <Box className="links-box desktop-menu">
+            {pages.map((page) => (
+              <Button
+                key={page.to}
+                component={Link}
+                to={page.to}
+                variant="contained"
+                color="secondary"
+              >
+                {page.label}
               </Button>
             ))}
           </Box>
 
-          <Box className="right">
-            <ColorModeSelect />
-            <IconButton aria-label="menu" onClick={handleMenuOpen}>
+          {/* -- COMMENTO -- Tre pallini spinti a DESTRA (solo mobile) */}
+          <Box className="mobile-menu right-menu">
+            <IconButton
+              aria-label="menu"
+              aria-controls={open ? 'nav-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleMenuOpen}
+            >
               <MoreVertIcon />
             </IconButton>
-            <Menu anchorEl={anchorEl} open={open} onClose={handleMenuClose}>
-              <MenuItem component={Link} to="/login" onClick={handleMenuClose}>Acced</MenuItem>
-              <MenuItem component={Link} to="/signup" onClick={handleMenuClose}>Registrati</MenuItem>
+            <Menu
+              id="nav-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleMenuClose}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.to}
+                  component={Link}
+                  to={page.to}
+                  onClick={handleMenuClose}
+                >
+                  {page.label}
+                </MenuItem>
+              ))}
             </Menu>
           </Box>
         </Toolbar>
